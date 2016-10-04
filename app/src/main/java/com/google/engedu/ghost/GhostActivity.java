4431d4efffc1b738fffd9f383cd307d84cb03f2d
RajuKoushik <g.rajukoushik@gmail.com>
2016-10-04 18:23:30 +0530

    Updated Unit 3 .

--- app/src/main/java/com/google/engedu/ghost/GhostActivity.java
+++ app/src/main/java/com/google/engedu/ghost/GhostActivity.java
@@ -3,9 +3,11 @@ package com.google.engedu.ghost;
 import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,22 +15,32 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Random;
 
+import static android.R.attr.keycode;
+
 
 public class GhostActivity extends AppCompatActivity {
     private static final String COMPUTER_TURN = "Computer's turn";
     private static final String USER_TURN = "Your turn";
     private GhostDictionary dictionary;
+    private SimpleDictionary simpleDictionary;
     private boolean userTurn = false;
     private Random random = new Random();
+    private TextView ghostText = (TextView)findViewById(R.id.ghostText);
+    private TextView gameStatus = (TextView)findViewById(R.id.gameStatus);
+    private Button bChallenge = (Button)findViewById(R.id.bChallenge);
+    private Button bRestart = (Button)findViewById(R.id.bRestart);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ghost);
+
+
         AssetManager assetManager = getAssets();
         try {
             InputStream inputStream = assetManager.open("words.txt");
             dictionary = new FastDictionary(inputStream);
+            simpleDictionary = new SimpleDictionary((inputStream));
         } catch (IOException e) {
             Toast toast = Toast.makeText(this, "Could not load dictionary", Toast.LENGTH_LONG);
             toast.show();
@@ -41,6 +53,28 @@ public class GhostActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_ghost, menu);
         return true;
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+
+        char unicodeChar = (char)event.getUnicodeChar();
+        String tempGhostText = ((String) ghostText.getText()).trim();
+
+        if(unicodeChar>='a' && unicodeChar<='z' || unicodeChar>='A' && unicodeChar<='Z')
+        {
+            tempGhostText = tempGhostText + unicodeChar ;
+            ghostText.setText(tempGhostText);
+            gameStatus.setText(COMPUTER_TURN);
+            return super.onKeyUp(keyCode, event);
+
+
+        }
+        else
+        {
+            return super.onKeyUp(keyCode, event);
+        }
+
     }
 
     @Override
@@ -83,5 +117,9 @@ public class GhostActivity extends AppCompatActivity {
         // Do computer turn stuff then make it the user's turn again
         userTurn = true;
         label.setText(USER_TURN);
+    }
+
+    public void ghostText(View view) {
+        //on click event handler for the ghostText TextView
     }
 }
